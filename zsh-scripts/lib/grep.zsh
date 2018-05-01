122b6f3ded6cdb2364073f281eda1211e452650c
Matthew Messmer <mmessmer@duoconsulting.com>
2018-05-01 12:15:29 +0900

    fix GREP_OPTIONS deprecated

--- zsh-scripts/lib/grep.zsh
+++ zsh-scripts/lib/grep.zsh
@@ -2,5 +2,7 @@
 # Color grep results
 # Examples: http://rubyurl.com/ZXv
 #
-export GREP_OPTIONS='--color=auto'
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
 export GREP_COLOR='1;32'
